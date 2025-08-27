78db60a29e6421bb77b6e910d9e084f59b34dc45
YePerdom <yoepardo@gmail.com>
2025-08-27 11:14:12 -0500

    update user.controllers.js

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,3 +1,4 @@
+import Role from "../models/role.js";
 import User from "../models/users.js";
 
 // Crear usuario
@@ -13,8 +14,30 @@ export async function createUser(req, res) {
 // Listar usuarios
 export async function getUsers(req, res) {
   try {
-    const users = await User.findAll();
+    const users = await User.findAll({
+      // Usar `include` en singular
+      include: [{
+        model: Role,
+        // Usar `required` para hacer un INNER JOIN
+        required: true,
+        attributes: [['name', 'role']] 
+      }]
+    });
     res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+//listar  usuario especifico
+export async function getUserById(req, res) {
+  try {
+    const id = req.params.id
+    const user = await User.findByPk(id);
+    if (!user){
+      res.status(404).json({message: "User not found"});
+    };
+    res.status(201).json(user);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
