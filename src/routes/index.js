a31ce533228bb7d378e5e017bcf7b0ee89881d94
YePerdom <yoepardo@gmail.com>
2025-08-29 11:44:43 -0500

    fix: Correct formatting of exports in routes index file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
 export { default as employees } from './emplyees.route.js';
 export { default as users } from './users.route.js';
-export {default as auth} from './auth.routes.js'
-export {default as certificates} from './certificates.route.js'
+export { default as auth } from './auth.routes.js'
+export { default as certificates } from './certificates.route.js'
