8ec5a5cc4c45071d57ee7470c31b2399e5696076
Yoelmis Enrique Perdomo Pardo <yoepardo@gmail.com>
2025-08-30 09:25:01 -0500

    feat: Add requests controller with total, by status, and last requests functionalities

--- src/controllers/reqests.controller.js
+++ src/controllers/reqests.controller.js
@@ -1,1 +1,83 @@
-import Recuest from '../models/'
+import { fn, col, literal } from 'sequelize';
+import Request from '../models/request.js';
+import Status from '../models/status.js';
+import Employee from '../models/employee.js';
+import RequestType from '../models/requestsType.js';
+import Priority from '../models/priority.js'
+
+export async function totalRequests(req, res) {
+    try {
+        const totalRequests = await Request.count();
+
+        if (totalRequests === 0) {
+            return res.status(200).json({ message: 'Not requests to show' })
+        }
+
+        res.status(200).json({
+            total_requests: totalRequests
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export async function requestsByStatus(req, res) {
+    try {
+        const requests = await Request.findAll({
+            attributes: [
+                [fn('COUNT', col('request.id')), 'total']
+            ],
+            include: [{
+                model: Status,
+                as: 'status',
+                attributes: ['codename']
+            }],
+            group: ['status.id', 'status.codename']
+        });
+
+        if (requests.length === 0) {
+            return res.status(200).json({ message: 'Not requests to show' });
+        }
+
+        const formatted = requests.reduce((acc, r) => {
+            acc[r.status.codename] = parseInt(r.getDataValue('total'), 10);
+            return acc;
+        }, {});
+
+        res.status(200).json(formatted);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export async function lastRequests(req, res) {
+    try {
+        const requests = await Request.findAll({
+            attributes: [
+                [fn('CONCAT', col('employee.name'), ' ', col('employee.lastname')), 'employee'],
+                [col('request_type.name'), 'request_type'],
+                [col('status.name'), 'status'],
+                [col('priority.name'), 'priority'],
+                [fn('TO_CHAR', col('request.created_at'), 'yyyy-MM-dd'), 'created_at']
+            ],
+            include: [
+                { model: Employee, as: 'employee', attributes: [] },
+                { model: Status, as: 'status', attributes: [] },
+                { model: RequestType, as: 'request_type', attributes: [] },
+                { model: Priority, as: 'priority', attributes: [] }
+            ],
+            order: [['created_at', 'DESC']],
+            limit: 4,
+            raw: true  
+        });
+
+        if (requests.length === 0) {
+            return res.status(200).json({ message: 'Not requests to show' });
+        }
+
+        res.status(200).json(requests);
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
