1f3aee93ebe66d084cea65e385fec6fda372b66b
Cesar Hernandez <cehergra1999@gmail.com>
2025-08-30 14:16:35 -0500

    endpoint request

--- src/routes/requests.route.js
+++ src/routes/requests.route.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { lastRequests, requestsByStatus, totalRequests } from '../controllers/reqests.controller.js';
+import { lastRequests, requestsByStatus, totalRequests, requestsByType } from '../controllers/reqests.controller.js';
 
 const router = Router();
 
 router.get('/total-requests', totalRequests);
 router.get('/requests-by-status', requestsByStatus);
 router.get('/last-requests', lastRequests)
+router.get('/type', requestsByType)
 
 export default router;
