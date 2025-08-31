13976a89e5e0e472dbd2656bda2a716181625eae
Yoelmis Enrique Perdomo Pardo <yoepardo@gmail.com>
2025-08-31 16:00:57 -0500

    Fix merge typo: corrected 'passworord' to 'password'
    - Resolved merge issue by fixing typo in field name.
    - Ensured all references consistently use 'password'

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -15,7 +15,7 @@ export async function createUser(req, res) {
 export async function getUsers(req, res) {
   try {
     const users = await User.findAll({
-      attributes: ['username', 'passworord', 'email'],
+      attributes: ['username', 'password', 'email'],
       include: [{
         model: Role,
         as: 'roles',
